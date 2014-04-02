87e594403b5943939dd500912905ccfebe8d1d4e
Ryan Gallus <rsgallus@Ryans-MacBook-Pro-9.local>
2014-04-02 11:26:32 -0500

    Commit 3

--- src/com/example/testapp/ScavengerHunt.java
+++ src/com/example/testapp/ScavengerHunt.java
@@ -1,6 +1,8 @@
 package com.example.testapp;
 
 import android.app.Activity;
+import com.google.android.gms.maps.*;
+import com.google.android.gms.maps.model.*;
 import android.os.Bundle;
 
 public class ScavengerHunt extends Activity {
@@ -10,6 +12,22 @@ public class ScavengerHunt extends Activity {
 	public void onCreate(Bundle savedInstanceState) {
 	    super.onCreate(savedInstanceState);
 	    setContentView(R.layout.scavenger_hunt);
+	    
+	 // Get a handle to the Map Fragment
+        GoogleMap map = ((MapFragment) getFragmentManager()
+                .findFragmentById(R.id.map)).getMap();
+        
+        //35:6A:80:C0:A3:4A:2C:AA:8D:D1:FC:7E:1F:0B:D5:39:80:61:92:58
+
+        LatLng sydney = new LatLng(-33.867, 151.206);
+
+        map.setMyLocationEnabled(true);
+        map.moveCamera(CameraUpdateFactory.newLatLngZoom(sydney, 13));
+
+        map.addMarker(new MarkerOptions()
+                .title("Sydney")
+                .snippet("The most populous city in Australia.")
+                .position(sydney));
 	}
 
 }
